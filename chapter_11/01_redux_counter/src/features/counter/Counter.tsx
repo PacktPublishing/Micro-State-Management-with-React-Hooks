a7e11055820b09e41f6ee8807b8198f32cec66c7
daishi <daishi@axlight.com>
2022-01-18 09:33:10 +0900

    fix

--- chapter_11/01_redux_counter/src/features/counter/Counter.tsx
+++ chapter_11/01_redux_counter/src/features/counter/Counter.tsx
@@ -1,30 +1,16 @@
 import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from "./counterSlice";
 
-type State = {
-  counter: {
-    value: number;
-  };
-};
-
 export function Counter() {
-  const count = useSelector((state: State) => state.counter.value);
+  const count = useSelector(
+    (state: { counter: { value: number } }) => state.counter.value
+  );
   const dispatch = useDispatch();
   return (
     <div>
-      <button
-        aria-label="Increment value"
-        onClick={() => dispatch(increment())}
-      >
-        Increment
-      </button>
+      <button onClick={() => dispatch(increment())}>Increment</button>
       <span>{count}</span>
-      <button
-        aria-label="Decrement value"
-        onClick={() => dispatch(decrement())}
-      >
-        Decrement
-      </button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   );
 }
--- chapter_11/02_zustand_counter/src/Counter.tsx
+++ chapter_11/02_zustand_counter/src/Counter.tsx
@@ -3,17 +3,12 @@ import { useStore } from "./store";
 export function Counter() {
   const count = useStore((state) => state.counter.value);
   const { increment, decrement } = useStore((state) => state.counterActions);
-
   return (
     <div>
       <div>
-        <button aria-label="Increment value" onClick={increment}>
-          Increment
-        </button>
+        <button onClick={increment}>Increment</button>
         <span>{count}</span>
-        <button aria-label="Decrement value" onClick={decrement}>
-          Decrement
-        </button>
+        <button onClick={decrement}>Decrement</button>
       </div>
     </div>
   );
